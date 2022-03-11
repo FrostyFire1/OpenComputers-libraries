4242581ecfe2e65d58ce7409a651085e7949d8e4
FrostyFire1 <kamilbaron02@gmail.com>
2022-03-11 13:03:37 +0100

    util.getItemNames() now prints a table with sum of each item in inventory instead of printing info of every slot

--- util.lua
+++ util.lua
@@ -6,19 +6,22 @@ function util.listMethods(component)
     for key,_ in pairs(component) do
         print(key)
     end
-
 end
 
 function util.getItemNames(component, side)
+    local itemList = {}
     if type(component) ~= "table" then
         return nil, "Component is " .. type(component) .. "And not a table" end
     if type(side) ~= "number" then
         return nil, "Side is " .. type(side) .. "And not a number" end
     for _, slotStack in pairs(component) do
-        if slotStack.name ~= "minecraft:air" then 
-            print(slotStack.name,"amount",slotStack.amount)
+        if slotStack.name ~= "minecraft:air" then
+            if itemList[slotStack.name] == nil then itemList[slotStack.name] = slotStack.amount
+            else itemList[slotStack.name] = itemList[slotStack.name] + slotStack.amount
+            end
         end
     end
+    return itemList
 end
 
 return util
